e24547105fdabe6a23f4f0e60636fdbef3a66858
Michel Anderson Lutz Teixeira <michel@mastersmx.com>
2017-07-12 15:41:45 -0300

    ajustes em comunicacao

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var SerialPort = require("serialport");
-const http = require("http");
-const fs = require("fs");
-const port = 3080;
+var serialConnection = null;
+const sp = "/dev/tty.usbmodem1411";
+const porta_ws = 3080;
+const baundrate = 9600
+try {
+    serialConnection = new SerialPort(sp, {
+      baundrate:baundrate
+    });
+} catch(error) {
+  console.log("Não foi possível acessar a porta: "+sp);
+}
 
-var port = new SerialPort("/dev/tty.usbmodem1411", {
-  baundrate:9600
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-port.on("open", function(){
-  console.log( "porta aberta");
-  port.write("0x00");
+io.on('connection', function(socket){
+  console.log('socket io connected id: ', socket.handshake.issued);
+  io.emit('send_serial_command', socket.handshake.issued+": conectou" );
 });
 
-port.on("data", function(byte){
-  console.log("DATA: ", byte);
-})
+http.listen(porta_ws, function(){
+  console.log('Webserver on http://localhost:'+porta_ws);
+});
+
+io.on('connection', function(socket){
+  socket.on('send_serial_command', function(_command){
+    console.log(socket.handshake.issued+': ' + _command);
+    console.log('serialConnection: ', serialConnection.opening);
+
+    io.emit('send_serial_command', socket.handshake.issued+": " +_command);
+    if(serialConnection.opening != false){
+      serialConnection.on("open", function(){
+        console.log("Porta serial aberta");
+        serialConnection.write(_command);
+      });
+      //Recebendo dados da serial
+      serialConnection.on("data", function(byte){
+        console.log("DATA: ", byte);
+        io.emit('send_serial_command', JSON.stringify({"retorno_serial": byte}));
+      });
+    } else {
+      console.log("Sem Conexão na porta: "+sp);
+    }
+  });
+});
